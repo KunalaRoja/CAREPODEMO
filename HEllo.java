fa6b64416821f744dfe8a602c6a5a3f423a90995
LTPB\U6049922 <roja.kunala@clarivate.com>
2020-02-11 16:10:24 +0530

    new ca repo branch programs

---
+++ HEllo.java
@@ -0,0 +1,11 @@
+public class HEllo{
+
+public static void main(String[] args){
+
+for(int i=0;i<=10;i++){
+
+System.out.println("Hello Clarivate Analytics");
+}
+}
+
+}
---
+++ ReversAString.java
@@ -0,0 +1,23 @@
+package StringPrograms;
+
+public class ReversAString {
+
+    public static void main(String args[]) {
+
+        String username="Rojakunala";
+        String reversename=" ";
+        for(int i=0;i<=username.length()-1;i++){
+
+            reversename=reversename+username.charAt(i);
+
+        //    System.out.println("reverse of ths string :"+reversename);
+
+
+
+        }
+
+
+    }
+
+
+}
